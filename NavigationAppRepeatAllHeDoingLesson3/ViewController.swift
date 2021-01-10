18f2417409ba60001c35eb9af85e8c6f69f09321
Vladimir <kynicin87@mail.ru>
2021-01-11 00:27:09 +0200

    Initial Commit

---
+++ NavigationAppRepeatAllHeDoingLesson3/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NavigationAppRepeatAllHeDoingLesson3
+//
+//  Created by vladimir gennadievich on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
